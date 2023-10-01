62c2298781e48f1ce1e0b1606833fc434a03c035
Ranjeet <ranjeetab17@gmail.com>
2023-10-01 09:11:30 +0530

    identation fix

--- DataAccess/DataModels/DataContext.cs
+++ DataAccess/DataModels/DataContext.cs
@@ -11,6 +11,7 @@ namespace DataAccess.DataModels
     public class DataContext : DbContext
     {
         public DataContext() { }
+
         public DataContext(DbContextOptions options) : base(options) { }
 
         public virtual DbSet<LengthConversion> LengthConversions { get; set; }
